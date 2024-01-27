2c5dd9b01319a14b24d76a7f0b158d7346982ae1
Antonio Obradović <antonio.obra8@gmail.com>
2024-01-27 10:34:26 +0100

    Init - Switched to T3 stack

--- coderepo/client/src/utils/jwt.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (userEmail: string): string => {
-    const secretKey = "your-secret-key"; // Replace with your actual secret key
-    const token = jwt.sign({ userEmail }, secretKey, { expiresIn: "1h" });
-
-    return token;
-};
