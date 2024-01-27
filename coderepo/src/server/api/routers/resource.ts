506c6b4516b93a55f79e24d0d643f43d8755d97e
Antonio Obradović <antonio.obra8@gmail.com>
2024-01-27 20:29:27 +0100

    [BE] - Added - update resource, create & update resource category, slugify

--- coderepo/src/server/api/routers/resource.ts
+++ coderepo/src/server/api/routers/resource.ts
@@ -6,6 +6,8 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
+import slugify from "slugify";
+
 export const resourceRouter = createTRPCRouter({
   getResourceCategories: publicProcedure.query(async ({ ctx }) => {
     const resourceCategories = await ctx.db.resourceCategory.findMany({
@@ -83,6 +85,56 @@ export const resourceRouter = createTRPCRouter({
     return resources;
   }),
 
+  getResourceById: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const resource = await ctx.db.resource.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          url: true,
+          category: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+
+      if (!resource) {
+        throw new Error("No resource found");
+      }
+
+      return resource;
+    }),
+
+  getResourceCategoryById: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const resourceCategory = await ctx.db.resourceCategory.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          icon: true,
+          slug: true,
+        },
+      });
+
+      if (!resourceCategory) {
+        throw new Error("No resource category found");
+      }
+
+      return resourceCategory;
+    }),
+
   createResource: protectedProcedure
     .input(
       z.object({
@@ -107,5 +159,93 @@ export const resourceRouter = createTRPCRouter({
       }
 
       return resource;
+    }),
+
+  updateResource: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        url: z.string(),
+        categoryId: z.number(),
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const resource = await ctx.db.resource.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          url: input.url,
+          categoryId: input.categoryId,
+        },
+      });
+
+      if (!resource) {
+        throw new Error("Resource not updated");
+      }
+
+      return resource;
+    }),
+
+  createResourceCategory: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        icon: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // Create a slug
+      const slug = slugify(input.name);
+
+      const resourceCategory = await ctx.db.resourceCategory.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          icon: input.icon,
+          slug: slug,
+        },
+      });
+
+      if (!resourceCategory) {
+        throw new Error("Resource category not created");
+      }
+
+      return resourceCategory;
+    }),
+
+  updateResourceCategory: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        icon: z.string(),
+        id: z.number(),
+        slug: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const resourceCategory = await ctx.db.resourceCategory.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          icon: input.icon,
+          slug: input.slug,
+        },
+      });
+
+      if (!resourceCategory) {
+        throw new Error("Resource category not updated");
+      }
+
+      return resourceCategory;
     }),
 });
