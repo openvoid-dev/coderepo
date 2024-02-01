1388c921864d6870a7e64d226696198235ca32b2
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-01 20:22:54 +0100

    [FE] - Added -  Loader for Resource Card  save feature

--- coderepo/src/app/resources/page.tsx
+++ coderepo/src/app/resources/page.tsx
@@ -5,22 +5,26 @@ import { api } from "~/trpc/server";
 import { type Icons } from "~/components/Icons";
 
 export default async function ResourceCategoriesPage() {
-    const resourceCategories = await api.resource.getResourceCategories.query();
+  const resourceCategories = await api.resource.getResourceCategories.query();
 
-    return (
-        <main className="pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
-            <PageHeader
-                heading="Curated Resources to Supercharge Your Web Development Journey"
-                text="Unlocking a Treasure Trove of Tools, Tutorials, and References for Seamless Web Development Success"
+  return (
+    <main className="pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
+      <PageHeader
+        heading="Curated Resources to Supercharge Your Web Development Journey"
+        text="Unlocking a Treasure Trove of Tools, Tutorials, and References for Seamless Web Development Success"
+      />
+
+      {resourceCategories.length > 0 && (
+        <section className="container mt-20 grid grid-cols-1 gap-6 md:grid-cols-4">
+          {resourceCategories.map((resource, index) => (
+            <ResourceCategoryCard
+              key={resource.slug + index}
+              {...resource}
+              icon={resource.icon as keyof typeof Icons}
             />
-
-            {resourceCategories.length > 0 && (
-                <section className="container grid grid-cols-1 md:grid-cols-3 mt-20 gap-6">
-                    {resourceCategories.map((resource, index) => (
-                        <ResourceCategoryCard key={resource.slug + index} {...resource} icon={resource.icon as keyof typeof Icons} />
-                    ))}
-                </section>
-            )}
-        </main>
-    )
+          ))}
+        </section>
+      )}
+    </main>
+  );
 }
--- coderepo/src/components/ResourceCard.tsx
+++ coderepo/src/components/ResourceCard.tsx
@@ -6,75 +6,113 @@ import { cn } from "~/lib/utils";
 import Link from "next/link";
 import { api } from "~/trpc/react";
 import { toast } from "sonner";
+import Loader from "~/components/Loader";
 
 export interface ResourceProps {
-    url: string;
-    name: string;
-    description: string;
-    id: number;
-    isSaved: boolean;
+  url: string;
+  name: string;
+  description: string;
+  id: number;
+  isSaved: boolean;
 }
 
-const ResourceCard = ({ url, name, description, id, isSaved }: ResourceProps) => {
-    const saveResource = api.user.saveResource.useMutation({
-        onSuccess: async () => {
-            toast("Resource saved successfully");
-        },
-        onError: (error) => {
-            toast(error.message);
-        },
-    });
+const ResourceCard = ({
+  url,
+  name,
+  description,
+  id,
+  isSaved,
+}: ResourceProps) => {
+  const saveResource = api.user.saveResource.useMutation({
+    onSuccess: async () => {
+      toast("Resource saved successfully");
+    },
+    onError: (error) => {
+      toast(error.message);
+    },
+  });
 
-    const unsaveResource = api.user.unsaveResource.useMutation({
-        onSuccess: async () => {
-            toast("Resource unsaved successfully");
-        },
-        onError: (error) => {
-            toast(error.message);
-        },
-    });
+  const unsaveResource = api.user.unsaveResource.useMutation({
+    onSuccess: async () => {
+      toast("Resource unsaved successfully");
+    },
+    onError: (error) => {
+      toast(error.message);
+    },
+  });
 
-    return (
-        <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-secondary via-background to-background border-2 border-muted rounded-lg p-8 relative group flex flex-col gap-8 h-full">
-            <hgroup className="flex flex-col">
-                <h1 className="text-2xl font-semibold text-foreground font-heading dark:text-primary-light mb-2 text-left">
-                    {name}
-                </h1>
-                <p className="text-md text-muted-foreground dark:text-muted-light text-left line-clamp-3">
-                    {description}
-                </p>
-            </hgroup>
+  return (
+    <div className="group relative flex h-full flex-col gap-8 rounded-lg border-2 border-muted bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-secondary via-background to-background p-8">
+      <hgroup className="flex flex-col">
+        <h1 className="dark:text-primary-light mb-2 text-left font-heading text-2xl font-semibold text-foreground">
+          {name}
+        </h1>
+        <p className="text-md dark:text-muted-light line-clamp-3 text-left text-muted-foreground">
+          {description}
+        </p>
+      </hgroup>
 
-            <div className="flex flex-col gap-4 mt-auto">
-                <div className="flex justify-between gap-4 mt-auto">
-                    {/* Save / Unsave Buttons */}
-                    {isSaved ? (
-                        <Button variant="ghost" className="flex items-center gap-2 flex-1" onClick={() => unsaveResource.mutate({ id: id })}>
-                            <Icons.star className="w-4 h-4 fill-white" />
-                            <span>Unsave</span>
-                        </Button>
-                    ) : (
-                        <Button variant="ghost" className="flex items-center gap-2 flex-1" onClick={() => saveResource.mutate({ id: id })}>
-                            <Icons.star className="w-4 h-4" />
-                            <span>Save</span>
-                        </Button>
-                    )}
+      <div className="mt-auto flex flex-col gap-4">
+        <div className="mt-auto flex justify-between gap-4">
+          {/* Save / Unsave Buttons */}
 
-                    <Button variant="ghost" className="flex items-center gap-2 flex-1" onClick={() =>
-                        navigator.clipboard.writeText(
-                            url
-                        )
-                    }>
-                        <Icons.copy className="w-4 h-4" />
-                        <span>Copy link</span>
-                    </Button>
-                </div>
+          {isSaved ? (
+            <>
+              {unsaveResource.isLoading ? (
+                <Loader
+                  className="flex flex-1 items-center justify-center px-4"
+                  svgClassName="h-6 w-6"
+                />
+              ) : (
+                <Button
+                  variant="ghost"
+                  className="flex flex-1 items-center gap-2"
+                  onClick={() => unsaveResource.mutate({ id: id })}
+                >
+                  <Icons.star className="h-4 w-4 fill-white" />
+                  <span>Unsave</span>
+                </Button>
+              )}
+            </>
+          ) : (
+            <>
+              {saveResource.isLoading ? (
+                <Loader
+                  className="flex flex-1 items-center justify-center px-4"
+                  svgClassName="h-6 w-6"
+                />
+              ) : (
+                <Button
+                  variant="ghost"
+                  className="flex flex-1 items-center gap-2"
+                  onClick={() => saveResource.mutate({ id: id })}
+                >
+                  <Icons.star className="h-4 w-4" />
+                  <span>Save</span>
+                </Button>
+              )}
+            </>
+          )}
 
-                <Link href={url} target="_blank" className={cn(buttonVariants({ variant: "secondary" }), " gap-3")}>
-                    <Icons.externalLink className="w-4 h-4" /> View Resource
-                </Link>
-            </div>
+          <Button
+            variant="ghost"
+            className="flex flex-1 items-center gap-2"
+            onClick={() => navigator.clipboard.writeText(url)}
+          >
+            <Icons.copy className="h-4 w-4" />
+            <span>Copy link</span>
+          </Button>
         </div>
-    );
+
+        <Link
+          href={url}
+          target="_blank"
+          className={cn(buttonVariants({ variant: "secondary" }), " gap-3")}
+        >
+          <Icons.externalLink className="h-4 w-4" /> View Resource
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default ResourceCard;
