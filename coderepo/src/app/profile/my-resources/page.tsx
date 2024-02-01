22959a13aa64a9321328081a9b347fb0956cebba
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-01 20:30:53 +0100

    [FE] - Added - Loader for profile my resources

--- coderepo/src/app/profile/my-resources/page.tsx
+++ coderepo/src/app/profile/my-resources/page.tsx
@@ -1,35 +1,25 @@
 "use client";
 
+import Loader from "~/components/Loader";
 import ResourceCard from "~/components/ResourceCard";
 import { api } from "~/trpc/react";
 
 export default function MyResourcesProfilePage() {
-    const { data: savedResources, isLoading } = api.user.getSavedResources.useQuery();
+  const { data: savedResources, isLoading } =
+    api.user.getSavedResources.useQuery();
 
-    if (isLoading) return (
-        <div>Loading</div>
-    );
+  return (
+    <main className="rounded-lg bg-secondary/20 px-8 py-10">
+      <div className="mb-6 flex items-center justify-between">
+        <h1 className="text-2xl font-semibold">My Resources</h1>
+      </div>
 
-    if (!savedResources) return (
-        <div>Not found</div>
-    );
-
-    return (
-        <main className="bg-secondary/20 rounded-lg py-10 px-8">
-            <div className="flex items-center justify-between mb-6">
-                <h1 className="text-2xl font-semibold">My Resources</h1>
-
-                {/* <Link href="/profile/admin/resources" className={cn(buttonVariants({ variant: "link" }))}>
-                <Icons.back className="w-5 h-5 mr-2" />
-                Go Back
-            </Link> */}
-            </div>
-
-            <div className="grid grid-cols-3 gap-6">
-                {savedResources.map((resource) => (
-                    <ResourceCard key={resource.name}  {...resource} isSaved={true} />
-                ))}
-            </div>
-        </main>
-    )
+      <div className="grid grid-cols-3 gap-6">
+        {isLoading && <Loader className="my-6" />}
+        {savedResources?.map((resource) => (
+          <ResourceCard key={resource.name} {...resource} isSaved={true} />
+        ))}
+      </div>
+    </main>
+  );
 }
