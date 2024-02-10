9cb9b31bbfb1405d665fe164d0fd49f92d53eed3
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-10 09:04:58 +0100

    [FE] - Added - guide image, guide content

--- coderepo/src/app/guides/[slug]/client.tsx
+++ coderepo/src/app/guides/[slug]/client.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
+import Markdown from "react-markdown";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
 import { Icons } from "~/components/Icons";
 import PageHeader from "~/components/PageHeader";
+import { CodeBlock, Pre } from "~/components/markdown/Code";
 import { buttonVariants } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 import { api } from "~/trpc/react";
@@ -24,10 +30,12 @@ interface SingleGuidesPageClientProps {
 }
 
 export default function SingleGuidesPageClient({ initialData, slug }: SingleGuidesPageClientProps) {
-    const { data, isLoading } = api.guide.getGuideBySlug.useQuery({ slug: slug }, {
+    const { data } = api.guide.getGuideBySlug.useQuery({ slug: slug }, {
         initialData: initialData,
         refetchOnMount: false
     });
+
+    const options = { code: CodeBlock, pre: Pre }
 
     if (!data) {
         return (
@@ -49,6 +57,26 @@ export default function SingleGuidesPageClient({ initialData, slug }: SingleGuid
     return (
         <>
             <PageHeader heading={data.name} text={data.description} />
+
+            <div className="max-w-5xl container rounded-lg overflow-hidden mt-10">
+                <Image src={data.imageUrl} alt={data.name} width="1000" height={500} className="rounded-lg" />
+            </div>
+
+            <article className="container max-w-3xl mt-20">
+                <Markdown
+                    className='prose prose-invert min-w-full'
+                    components={options}
+                    remarkPlugins={[remarkGfm]}
+                    rehypePlugins={[
+                        rehypeSanitize,
+                        [rehypeExternalLinks,
+                            { content: { type: 'text', value: '🔗' } }
+                        ],
+                    ]}
+                >
+                    {data.content}
+                </Markdown>
+            </article>
         </>
     )
 }
