437786639fa56cd27bcaf5488682cf8dcf5fa3d2
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-10 11:18:46 +0100

    [FE] - Added - google provider (hidden for now), removed credentials provider

--- coderepo/src/app/signin/SignInForm.tsx
+++ coderepo/src/app/signin/SignInForm.tsx
@@ -1,81 +1,24 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { useState, } from "react"
 
-import { Button, buttonVariants } from "~/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "~/components/ui/form"
-import { Input } from "~/components/ui/input"
+import Loader from "~/components/Loader"
+import { Icons } from "~/components/Icons"
+import { Button } from "~/components/ui/button"
+
+import { toast } from "sonner"
 import { signIn } from "next-auth/react"
 
-import { useRouter } from 'next/navigation';
-import Link from "next/link"
-import { cn } from "~/lib/utils"
-import { useState, type BaseSyntheticEvent } from "react"
-import { Icons } from "~/components/Icons"
-import { toast } from "sonner"
-
-const signInSchema = z.object({
-    email: z.string().email({
-        message: "Please enter a valid email.",
-    }),
-    password: z.string().min(8, {
-        message: "Password must be at least 8 characters.",
-    }),
-})
-
-type FormFields = z.infer<typeof signInSchema>
 
 const SignInForm = () => {
-    const router = useRouter();
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const form = useForm<FormFields>({
-        resolver: zodResolver(signInSchema),
-        defaultValues: {
-            email: "",
-            password: ""
-        },
-    })
-
-    const onSubmit = async (data: FormFields, event: BaseSyntheticEvent | undefined) => {
-        event?.preventDefault();
-
-        try {
-            // * Sign in the user
-            const response = await signIn("credentials", {
-                redirect: false,
-                email: data.email,
-                password: data.password,
-            })
-
-            if (response?.error) {
-                throw new Error(response.error)
-            }
-
-            // * Redirect to profile page
-            router.push('/profile');
-            router.refresh();
-
-        } catch (error) {
-            console.error("Authentication error:", (error as Error).message);
-        }
-    };
-
-    const onGithubSignIn = async () => {
+    const onAuthSignIn = async (provider: string) => {
         setIsLoading(true);
 
         try {
-            await signIn("github", { redirect: true, callbackUrl: "/profile" })
+            await signIn(provider, { redirect: true, callbackUrl: "/profile" })
+            toast("You have been successfully signed in. Redirecting to your profile.")
         } catch (error) {
             // TODO: Set error message with toast
             toast("Event has been created.")
@@ -84,60 +27,20 @@ const SignInForm = () => {
         }
     }
 
+    if (isLoading) (
+        <Loader />
+    )
+
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4" autoComplete="off">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="coderepo@obradovic.dev" {...field} />
-                            </FormControl>
-                            <FormMessage className="text-red-500" />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input placeholder="********" type="password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+        <div className="flex flex-col gap-6">
+            <Button className="w-full" variant="secondary" type="button" onClick={() => onAuthSignIn("github")}>
+                <Icons.gitHub className="w-5 h-5 mr-4" /> Sign in with GitHub
+            </Button>
 
-                <FormDescription>
-                    <Link href="#" className="text-muted-foreground hover:underline">
-                        Forgot password?
-                    </Link>
-                </FormDescription>
-                <Button type="submit" className="w-full" >Sign In</Button>
-
-                <FormDescription className="text-center">
-                    <span className="text-gray-600">or</span>
-                </FormDescription>
-
-                <Button className="w-full" variant="secondary" type="button" onClick={onGithubSignIn}>
-                    <Icons.gitHub className="w-5 h-5 mr-4" /> Sign in with GitHub
-                </Button>
-
-                <FormDescription className="text-center">
-                    Don&apos;t have an account?{" "}
-
-                    <Link href="/signup" className={cn(buttonVariants({ variant: "link" }), "p-0")}>
-                        Sign up
-                    </Link>
-                </FormDescription>
-            </form>
-        </Form>
+            {/* <Button className="w-full" variant="secondary" type="button" onClick={() => onAuthSignIn("google")}>
+                <Icons.google className="w-5 h-5 mr-4" /> Sign in with Google
+            </Button> */}
+        </div>
     )
 }
 
