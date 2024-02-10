1576cf65cefd9f0da6e645cb17b92526594f346c
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-10 11:30:04 +0100

    [FE] - Added - prefetching resources for admin

--- coderepo/src/app/profile/admin/resources/page.tsx
+++ coderepo/src/app/profile/admin/resources/page.tsx
@@ -1,47 +1,11 @@
-import Link from "next/link";
-import { Icons } from "~/components/Icons";
-import { resourceColumns } from "~/components/admin/Resource/columns";
-import { ResourceDataTable } from "~/components/admin/Resource/data-table";
-import { resourceCategoryColumns } from "~/components/admin/ResourceCategory/columns";
-import { ResourceCategoryDataTable } from "~/components/admin/ResourceCategory/data-table";
-import { buttonVariants } from "~/components/ui/button";
-import { cn } from "~/lib/utils";
 import { api } from "~/trpc/server"
+import AdminResourcesPageClient from "~/app/profile/admin/resources/client";
 
 export default async function AdminResourcesPage() {
-    const allResourceCategories = await api.resource.getResourceCategories.query();
-    const allResources = await api.resource.getResources.query();
+    const initialAllResourceCategories = await api.resource.getResourceCategories.query();
+    const initalAllResources = await api.resource.getResources.query();
 
     return (
-        <main>
-            {/* Resource Categories */}
-            <section className="">
-                <div className="flex justify-between">
-                    <h2 className="font-bold font-heading text-left text-2xl">
-                        Edit Resource Categories
-                    </h2>
-
-                    <Link href="/profile/admin/resources/categories/add" className={cn(buttonVariants({ variant: "link" }))}>
-                        <Icons.create className="w-5 h-5 mr-2" /> Add Resource Category
-                    </Link>
-                </div>
-                <ResourceCategoryDataTable columns={resourceCategoryColumns} data={allResourceCategories} />
-            </section>
-
-            {/* Resources */}
-            <section className="mt-4">
-                <div className="flex justify-between">
-                    <h2 className="font-bold font-heading text-left text-2xl">
-                        Edit Resources
-                    </h2>
-
-                    <Link href="/profile/admin/resources/add" className={cn(buttonVariants({ variant: "link" }))}>
-                        <Icons.create className="w-5 h-5 mr-2" /> Add Resource
-                    </Link>
-                </div>
-
-                <ResourceDataTable columns={resourceColumns} data={allResources} />
-            </section>
-        </main>
+        <AdminResourcesPageClient initalAllResources={initalAllResources} initialAllResourceCategories={initialAllResourceCategories} />
     )
 }
