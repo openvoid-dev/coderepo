11e0462b3932c36cb175c4fb99a01ef2956aad04
Antonio Obradović <antonio.obra8@gmail.com>
2024-02-10 14:21:16 +0100

    [FE] - Added - template single page, template navigation for template pages

--- coderepo/src/app/templates/[slug]/client.tsx
+++ coderepo/src/app/templates/[slug]/client.tsx
@@ -1,7 +1,74 @@
-export default async function SingleTemplatesPageClient() {
+"use client";
+
+import Link from "next/link";
+import Markdown from "react-markdown";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+import { Icons } from "~/components/Icons";
+import PageHeader from "~/components/PageHeader";
+import { CodeBlock, Pre } from "~/components/markdown/Code";
+import { Button, buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+import { api } from "~/trpc/react";
+
+interface SingleTemplatesPageClientProps {
+    initialTemplate: {
+        id: number;
+        name: string;
+        description: string;
+        slug: string;
+        githubUrl: string;
+        content: string;
+        createdAt: Date;
+        updatedAt: Date;
+    };
+}
+
+export default function SingleTemplatesPageClient({ initialTemplate }: SingleTemplatesPageClientProps) {
+    const { data: template } = api.template.getTemplateBySlug.useQuery({ slug: initialTemplate.slug }, { initialData: initialTemplate, refetchOnMount: false });
+
+    const options = { code: CodeBlock, pre: Pre }
     return (
         <div>
-            <h1>Single Template Page</h1>
+            <PageHeader
+                heading="Inspiring Templates to Use"
+                text="Exploring the Possibilities of Cutting-Edge Web Development Technologies: Showcasing Innovative and Inspiring Projects"
+            />
+
+            <div className="mt-8">
+                {/* <p className="text-center">This template is part of the <Link href="" className={cn(buttonVariants({ variant: "link" }), "p-0 text-md")}>Next.js handbook</Link>.</p> */}
+
+                <div className="flex items-center justify-center gap-4 mt-4">
+                    <Link href="" className={cn(buttonVariants({ variant: "default", size: "lg" }))}>
+                        <Icons.docs className="w-5 h-5 mr-3" /> Get Started
+                    </Link>
+
+                    <Link href={"githubUrl"} target="_blank" className={cn(buttonVariants({ variant: "secondary", size: "lg" }))}>
+                        <Icons.gitHub className="w-5 h-5 mr-3" /> Github
+                    </Link>
+
+                    <Button variant="secondary" size="lg">
+                        <Icons.star className="w-5 h-5 mr-3" /> Save
+                    </Button>
+                </div>
+            </div>
+
+            <div className="mt-14">
+                <Markdown
+                    className='prose prose-invert min-w-full'
+                    components={options}
+                    remarkPlugins={[remarkGfm]}
+                    rehypePlugins={[
+                        rehypeSanitize,
+                        [rehypeExternalLinks,
+                            { content: { type: 'text', value: '🔗' } }
+                        ],
+                    ]}
+                >
+                    {template?.content}
+                </Markdown>
+            </div>
         </div>
     )
 }
--- coderepo/src/app/templates/[slug]/layout.tsx
+++ coderepo/src/app/templates/[slug]/layout.tsx
@@ -1,15 +1,30 @@
+import { notFound } from "next/navigation";
+import TemplateNavigation from "~/components/TemplateNavigation";
+import { api } from "~/trpc/server";
+
 export default async function SingleTemplatesPageLayout({
-    children,
+    children, params
 }: {
     children: React.ReactNode;
+    params: {
+        slug: string;
+    };
 }) {
+    const intialTemplate = await api.template.getTemplateBySlug.query({ slug: params.slug });
+
+    if (!intialTemplate) {
+        notFound();
+    }
+
+    const initialTemplatePages = await api.template.getTemplatePages.query({ id: intialTemplate.id });
+
     return (
         <main className="container flex gap-6">
+
             <div className="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10 container">
                 {/* Template Navbar */}
-                <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10">
-                    <div className="w-full"><div className="pb-8"><h4 className="mb-1 rounded-md px-2 py-1 text-sm font-medium">Getting Started</h4><div className="grid grid-flow-row auto-rows-max text-sm"><a className="flex w-full items-center rounded-md p-2 hover:underline" target="" rel="" href="/docs">Introduction</a></div></div><div className="pb-8"><h4 className="mb-1 rounded-md px-2 py-1 text-sm font-medium">Documentation</h4><div className="grid grid-flow-row auto-rows-max text-sm"><a className="flex w-full items-center rounded-md p-2 hover:underline bg-muted" target="" rel="" href="/docs/documentation">Introduction</a><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Contentlayer</span><a className="flex w-full items-center rounded-md p-2 hover:underline" target="" rel="" href="/docs/documentation/components">Components</a><a className="flex w-full items-center rounded-md p-2 hover:underline" target="" rel="" href="/docs/documentation/code-blocks">Code Blocks</a><a className="flex w-full items-center rounded-md p-2 hover:underline" target="" rel="" href="/docs/documentation/style-guide">Style Guide</a><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Search</span></div></div><div className="pb-8"><h4 className="mb-1 rounded-md px-2 py-1 text-sm font-medium">Blog</h4><div className="grid grid-flow-row auto-rows-max text-sm"><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Introduction</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Build your own</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Writing Posts</span></div></div><div className="pb-8"><h4 className="mb-1 rounded-md px-2 py-1 text-sm font-medium">Dashboard</h4><div className="grid grid-flow-row auto-rows-max text-sm"><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Introduction</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Layouts</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Server Components</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Authentication</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">Database with Prisma</span><span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">API Routes</span></div></div></div>
-                </aside>
+                <TemplateNavigation initialTemplatePages={initialTemplatePages} templateId={intialTemplate.id} templateSlug={params.slug} />
+
 
                 {/* Content */}
                 <section className="relative py-6 lg:gap-10 lg:py-10">
